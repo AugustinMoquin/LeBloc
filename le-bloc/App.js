0641ae096d56036a1c4020c6b9f09d511c53731a
AugustinMoquin <augustinmqn@gmail.com>
2023-12-08 16:30:21 +0100

    debut d'architecture de l'app

--- le-bloc/App.js
+++ le-bloc/App.js
@@ -1,10 +1,32 @@
+import React, {useCallback} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import {FONTS} from './src/styles';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    RobotoReg: Roboto_400Regular,
+    RobotoMed: Roboto_500Medium, 
+    RobotoBold: Roboto_700Bold
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={FONTS.h3_Bold}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ le-bloc/src/styles/index.js
@@ -0,0 +1,55 @@
+const SIZES = {
+    paddng: 15,
+    icon: 25,
+    h1: 24,
+    h2: 20,
+    h3: 18,
+    h4: 16,
+    h5: 14,
+    h6: 13,
+    h7: 12
+}
+
+const COLORS = {
+    primary: "#3B0000",
+    secondary: "#FEF9DE",
+    white: "#fff",
+    white_bg: "",
+    grey: "#707070"
+}
+
+const FONTS = {
+    h1_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h1},
+    h1_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h1},
+    h1_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h1},
+    h2_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h2},
+    h2_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h2},
+    h2_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h2},
+    h3_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h3},
+    h3_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h3},
+    h3_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h3},
+    h4_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h4},
+    h4_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h4},
+    h4_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h4},
+    h5_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h5},
+    h5_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h5},
+    h5_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h5},
+    h6_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h6},
+    h6_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h6},
+    h6_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h6},
+    h7_Bold: {fontFamily: "RobotoBold", fontSize: SIZES.h7},
+    h7_Med: {fontFamily: "RobotoMed", fontSize: SIZES.h7},
+    h7_Reg: {fontFamily: "RobotoReg", fontSize: SIZES.h7},
+}
+
+const SHADOW = {
+    shadow_color: "rgba:(0,0,0,0.5)",
+    shadow_offset: {
+        width: 8,
+        height: 4
+    },
+    shadow_opacity: 1,
+    shadow_radius: 15
+}
+
+export {SIZES, COLORS, FONTS, SHADOW}
