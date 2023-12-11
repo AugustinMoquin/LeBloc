8ff8f516d9d4cd5e6c38e4444e4dab558e301f2b
AMOQUIN <augustin.moquin@ynov.com>
2023-12-11 16:37:38 +0100

    ajout du css

--- le-bloc/src/screens/login.js
+++ le-bloc/src/screens/login.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { React } from "react";
+import style from 'le-bloc/src/styles/login_style.css';
 
-const styles= StyleSheet.create({
-    container: {
-        height: '100%',
-        width: '100%',
-    }
-})
-
-export default function login() {
+const AndroidLarge4 = () => {
   return (
-    <View style={styles.container}></View>
-  )
-}
+    <div className="android-large-4" style={style.AndroidLarge4}>
+      <img className="android-large-4-child" alt="" src="/group-5@2x.png" />
+      <img className="android-large-4-item" alt="" src="/rectangle-16.svg" />
+      <div className="android-large-4-inner" />
+      <img className="texture-icon" alt="" src="le-bloc/src/img/Texture.png" />
+      <div className="frame-parent">
+        <div className="rectangle-parent">
+          <div className="frame-child" />
+          <div className="frame-item" />
+          <img className="mousqueton-icon" alt="" src="le-bloc/src/img/Mousqueton.png" />
+          <div className="je-reviens-de">Je reviens de loin</div>
+        </div>
+        <div className="rectangle-group">
+          <div className="frame-child" />
+          <div className="rectangle-div" />
+          <img className="chaussure-icon" alt="" src="le-bloc/src/img/Chaussure.png" />
+          <div className="je-suis-nouveau">Je suis Nouveau</div>
+        </div>
+      </div>
+      <div className="bienvenue-parent">
+        <div className="bienvenue">Bienvenue</div>
+        <img className="line-icon" alt="" src="/line-1.svg" />
+      </div>
+      <img className="logo-icon" alt="" src="/logo.jpg" />
+    </div>
+  );
+};
+
+export default AndroidLarge4;
+
